756d0978bb809b010ddcbe13a5eaf53b8ec6bccd
cl19970520 <893182141@qq.com>
2024-05-20 18:04:57 +0800

    feat: 添加路由

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,17 +2,24 @@ import { lazy } from "react";
 import { createBrowserRouter, RouteObject } from "react-router-dom";
 import lazyLoad from "./lazyLoad";
 
+
+export declare type MenuRouteObject = {
+    label?: string;
+} & RouteObject
+
 const childrenRoutes: RouteObject[] = [
-    { path: "user", element: lazyLoad(lazy(() => import("@/pages/User"))) },
+  { path: "user", element: lazyLoad(lazy(() => import("@/pages/User"))) },
 ];
-const routers: RouteObject[] = [
+
+const routers: MenuRouteObject[] = [
   {
     path: "/",
+    label: "首页",
     element: lazyLoad(lazy(() => import("@/pages/Home/Home"))),
     children: childrenRoutes,
   },
 ];
-
+console.log(routers);
 const router = createBrowserRouter(routers);
 
 export { router };
