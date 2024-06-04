b8763de22152199c1feea5946f4b50a9c287d8ea
cl19970520 <893182141@qq.com>
2024-06-04 18:02:14 +0800

    feat: 配合接口调用

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,11 +5,23 @@ import DayTaskList from "./components/DayTask.tsx";
 import UserInfo from "./components/UserInfo.tsx";
 import homeCss from "./Home.module.css";
 const Home: React.FC = () => {
-  fetch("/api/v1/cats/bread", {
+  const formData = new FormData();
+  formData.append("name", "陈林");
+  fetch("/api/v1/cats", {
     method: "GET",
+    // body: JSON.stringify({
+    //   name: "陈林",
+    //   age: 100,
+    //   breed: "测试",
+    // }),
+    // headers: {
+    //   "Content-Type": "application/json",
+    // },
   }).then((res) => {
-    console.log(res);
-  });
+    return res.json();
+  }).then(res=>{
+    console.log(res)
+  })
   return (
     <HomeBackground>
       <div className={homeCss.home_body}>
