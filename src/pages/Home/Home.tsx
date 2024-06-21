b59640b2567562031718bd67c898369be49a3c8e
cl19970520 <893182141@qq.com>
2024-06-21 17:06:27 +0800

    feat: 添加CRUD

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -37,6 +37,7 @@ const Home: React.FC = () => {
     username: "",
     password: "",
     code: "",
+    id: "",
   });
 
   const [codeRandom, setCodeRandom] = useState(0);
@@ -74,7 +75,58 @@ const Home: React.FC = () => {
       body: formData,
     });
   };
+  const handleDownload = () => {
+    fetch("/api/v1/upload/stream")
+      .then((res) => res.blob())
+      .then((res) => {
+        console.log(res);
+        const a = document.createElement("a");
+        // const file = URL.createObjectURL(new Blob([res], { type: "application/zip" }));
+        const file = URL.createObjectURL(res);
 
+        a.href = file;
+        a.download = "test.zip";
+        a.click();
+      });
+  };
+
+  const handeCreate = () => {
+    fetch("/api/v1/record", {
+      method: "POST",
+      body: JSON.stringify({
+        name: "陈林",
+        age: 18,
+        sex: "male",
+        descption: "测试",
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
+  const handeSearch = () => {
+    fetch("/api/v1/record?keyWord=&page=1&size=10", {
+      method: "GET",
+    });
+  };
+
+  const handeDelete = () => {
+    fetch("/api/v1/record/" + userInfo.id, {
+      method: "DELETE",
+    });
+  };
+
+  const handeUpdate = () => {
+    fetch("/api/v1/record/" + userInfo.id, {
+      method: "PATCH",
+      body: JSON.stringify({
+        name: "王体先",
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
   return (
     <HomeBackground>
       <div className={homeCss.home_body}>
@@ -118,6 +170,22 @@ const Home: React.FC = () => {
           <button onClick={handleLogin}>提交</button>
 
           <input type="file" name="file" onChange={handleUpload} />
+
+          <button onClick={handleDownload}>下载图片</button>
+
+          <button onClick={handeCreate}>新增记录</button>
+          <button onClick={handeSearch}>查询记录</button>
+
+          <input
+            type="text"
+            name="id"
+            value={userInfo.id}
+            onChange={handleChange}
+            placeholder="请输入id"
+          />
+          <button onClick={handeDelete}>删除记录</button>
+
+          <button onClick={handeUpdate}>更新记录</button>
         </div>
       </div>
       <Outlet />
